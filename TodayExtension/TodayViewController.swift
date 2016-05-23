7a96df893203c0516f092703a111b4273cdf740e
imk2o <imk2o@icloud.com>
2016-05-23 14:09:18 +0900

    Today Extensionのサンプルを実装

--- TodayExtension/TodayViewController.swift
+++ TodayExtension/TodayViewController.swift
@@ -8,9 +8,16 @@
 
 import UIKit
 import NotificationCenter
+import CoreMotion
 
 class TodayViewController: UIViewController, NCWidgetProviding {
         
+    @IBOutlet weak var stepsLabel: UILabel!
+    
+    private let pedometer: CMPedometer = {
+        return CMPedometer()
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view from its nib.
@@ -21,16 +28,37 @@ class TodayViewController: UIViewController, NCWidgetProviding {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func viewDidTap(sender: AnyObject) {
+        // URLスキームでアプリを起動
+        if let appURL = NSURL(string: "myapp://") {
+            self.extensionContext?.openURL(appURL, completionHandler: nil)
+        }
+    }
+    
+    // MARK - NCWidgetProviding
+    
     func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)) {
-        // Perform any setup necessary in order to update the view.
+        guard CMPedometer.isStepCountingAvailable() else {
+            completionHandler(.Failed)
+            return
+        }
 
-        // FIXME
-        
-        // If an error is encountered, use NCUpdateResult.Failed
-        // If there's no update required, use NCUpdateResult.NoData
-        // If there's an update, use NCUpdateResult.NewData
+        // 今日の00:00から現時刻までの歩数を求め、これを表示する
+        let now = NSDate()
+        let calendar = NSCalendar.currentCalendar()
+        let dateComponents = calendar.components([NSCalendarUnit.Year, NSCalendarUnit.Month, NSCalendarUnit.Day], fromDate: now)
+        let startDate = calendar.dateFromComponents(dateComponents)
 
-        completionHandler(.NewData)
+        self.pedometer.queryPedometerDataFromDate(startDate!, toDate: now) { (data, error) in
+            if let _ = error {
+                completionHandler(.Failed)
+            } else if let steps = data?.numberOfSteps {
+                self.stepsLabel.text = "\(steps) steps"
+                completionHandler(.NewData)
+            } else {
+                completionHandler(.NoData)
+            }
+        }
     }
 
     // 通知センター内に確保されるマージンを調整したい場合に実装する
